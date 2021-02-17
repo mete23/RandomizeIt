a86f64b9488d4216e783df1498d72bac35143618
mete23 <mete.yayla@web.de>
2021-02-17 14:15:00 +0100

    ListsPanelMain and RandomButton added
    +Cardlayout added to ListPanelMain

--- RandomizeIt/src/ListsPanel.java
+++ RandomizeIt/src/ListsPanel.java
@@ -1,4 +1,7 @@
 import javax.swing.JPanel;
+
+import java.awt.CardLayout;
+
 import javax.swing.JLabel;
 public class ListsPanel extends JPanel{
 
@@ -6,9 +9,17 @@ public class ListsPanel extends JPanel{
 	private static final long serialVersionUID = -2768123984254904084L;
 	JPanel listsPanel;
 	JLabel testText;
-	
+	CardLayout listsCardLayout;
+	ListsPanelMain listsPanelMain;
 	public ListsPanel() {
 		listsPanel = new JPanel();
+		listsPanelMain = new ListsPanelMain();
+		this.listsCardLayout = new CardLayout();
+		listsPanel.setLayout(this.listsCardLayout);
+		listsPanel.add(listsPanelMain, "listsPanelMain");
+		
+		this.listsCardLayout.show(listsPanel, "listsPanelMain");
+		
 		
 		add(listsPanel);
 	}
