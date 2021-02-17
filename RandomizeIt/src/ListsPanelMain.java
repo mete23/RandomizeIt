08c4afccc4e8364a4ee639f5db1bbe551700aebf
mete23 <mete.yayla@web.de>
2021-02-17 20:17:27 +0100

    added JTextField to ListsPanelMain

--- RandomizeIt/src/ListsPanelMain.java
+++ RandomizeIt/src/ListsPanelMain.java
@@ -1,4 +1,5 @@
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -10,12 +11,21 @@ public class ListsPanelMain extends JPanel implements ActionListener{
 	private static final long serialVersionUID = 7101308713798768031L;
 	JPanel listsPanelMain;
 	JButton randomButton;
+	JTextField randomResult;
+
 	public ListsPanelMain() {
 		listsPanelMain = new JPanel();
 		randomButton = new JButton("random");
 		randomButton.addActionListener(this);
 		
+		randomResult = new JTextField();
+		randomResult.setEditable(false);
+		
+		randomResult.setText("Result");
+		
 		listsPanelMain.add(randomButton);
+		listsPanelMain.add(randomResult);
+		
 		
 		add(listsPanelMain);
 		
