1e80acc5a1dce5f48422da31842f45f681dd6a02
mete23 <mete.yayla@web.de>
2021-06-02 20:30:21 +0200

    changed folder source

--- RandomizeIt/src/Data.java
+++ RandomizeIt/src/Data.java
@@ -8,7 +8,7 @@ public class Data {
 	File folder;
 	File file;
 	
-	String defaultFolder = "Data";
+	String defaultFolder = "Data/Lists";
 	
 	private ArrayList<String> names = new ArrayList<String>();
 	
